63eaea8b69c311df282e95eaaf877c6efe68915b
cxoltero <carlosh95@gmail.com>
2014-10-15 21:19:00 -0400

    assined user ids

--- reddit/app/controllers/links_controller.rb
+++ reddit/app/controllers/links_controller.rb
@@ -1,5 +1,6 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_filter :authenticate_user!, except: [:index, :show]
 
   # GET /links
   # GET /links.json
@@ -13,8 +14,10 @@ class LinksController < ApplicationController
   end
 
   # GET /links/new
+
+  #the current_user.links.build will build from each user.
   def new
-    @link = Link.new
+    @link = current_user.links.build
   end
 
   # GET /links/1/edit
@@ -24,7 +27,7 @@ class LinksController < ApplicationController
   # POST /links
   # POST /links.json
   def create
-    @link = Link.new(link_params)
+    @link = current_user.links.build(link_params)
 
     respond_to do |format|
       if @link.save
