28c35577124fa92f625fcbcf42501f12753012bb
cxoltero <carlosh95@gmail.com>
2014-10-15 23:51:47 -0400

    added comments scaffold and the migration for it

--- reddit/db/schema.rb
+++ reddit/db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141016025839) do
+ActiveRecord::Schema.define(version: 20141016035115) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: true do |t|
+    t.integer  "link_id"
+    t.text     "body"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["link_id"], name: "index_comments_on_link_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "links", force: true do |t|
     t.string   "title"
